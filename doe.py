83514c8c555f086d30adedb0c8d6b39a86080190
Matthew F Leader <mleader@redhat.com>
2021-09-24 13:16:30 +0000

    remove k8s code from doe.doe

--- doe.py
+++ doe.py
@@ -1,15 +1,9 @@
 import itertools
-import json
-import sys
-from pathlib import Path
 import random
 import uuid
 import toml
 
-from kubernetes import client, config
 import pandas as pd
-
-import ryaml
 
 
 def unchanged_levels(factor_levels):
@@ -28,101 +22,37 @@ def unchanged_levels(factor_levels):
 def randomize_powerset(factor_levels):
     random.seed(1)
     power_set_levels = list(itertools.product(*factor_levels.values()))
-    # random.shuffle(power_set_levels)
-    # print(power_set_levels)
+    random.shuffle(power_set_levels)
     df0 = pd.DataFrame.from_records(power_set_levels, columns=factor_levels.keys())
     df0['trial_id'] = df0.index + 1
-    # print(df0.sample(frac=1,random_state=1))
+    print(f"Total trials to run: {len(df0.index)}")
     for exp_levels in df0.to_dict('records'):
         yield exp_levels
 
 
-def create_pod(env):
-    return client.V1Pod(
-        api_version = "v1",
-        kind = "Pod",
-        metadata = client.V1ObjectMeta(
-            name = env['name'],
-            namespace = env['namespace']
-        ),
-        spec = client.V1PodSpec(
-            containers = [
-                client.V1Container(
-                    name = env['container']['name'],
-                    image = env['container']['image'],
-                    image_pull_policy = 'Always',
-                    command = env['container']['command'],
-                    args = env['container']['args']
-                )
-            ],
-            restart_policy = 'Never'
-        )
-    )
-
-
 def serialize_command_args(cmd_args: dict):
     for key, val in cmd_args.items():
-        yield '--' + str(key)
+        yield '--' + str(key.replace('_', '-'))
         yield str(val)
 
 
 def main(
-    # es_url, es_index,
     factor_levels_filepath,
     block_id = 1,
 ):
-    # elasticsearch_url = sys.argv[2]
-    # with open(factor_levels_filepath, 'r') as factor_levels_file:
     factor_levels_dict = toml.load(factor_levels_filepath)
-    # print(factor_levels_dict)
-    #     factor_levels_dict = json.load(factor_levels_file)
-
-    env = {
-        'name': 'dnsperf-test-wrapper',
-        'namespace': 'dnsperf-test',
-        'container': {
-            'name': 'container',
-            'image': 'quay.io/mleader/dnsdebug:latest',
-            'command': ['/bin/sh', '-c'],
-            'args': None
-        }
-    }
-
     base_args, factor_levels = unchanged_levels(factor_levels_dict)
-    base_cmd = [
-        'snafu/run_snafu.py',
-        '-v'
-    ]
-
     base_args = {
         'run_id': str(uuid.uuid4()),
         'block_id': block_id,
         **base_args
     }
 
-
-    # print(base_args_levels)
-
     for trial in randomize_powerset(factor_levels):
-        # env['container']['args'] = [
-        #     *base_args_levels,
-        #     *list(serialize_command_args(trial))
-        # ]
-        # print(env['container']['args'])
-        # print(trial)
         yield {
             **base_args, **trial
         }
-        # mypod = create_pod(env)
-        # with open(f"ocp_apps/{'-'.join((str(val) for val in trial.values()))}.yaml", 'w') as ocp_app:
-            # ocp_app.write(ryaml.dumps(client.ApiClient().sanitize_for_serialization(mypod)))
-
-
-
-
-
 
 
 if __name__ == '__main__':
     main()
-
--- main_k8s.py
+++ main_k8s.py
@@ -1,28 +1,20 @@
-import subprocess
-import sys
 import time
-import shutil
-from pathlib import Path
-import os
 from pprint import pprint
-
+import asyncio
+from functools import partial
 import typer
 from kubernetes import client, config, watch
+from kubernetes.dynamic import DynamicClient
+from kubernetes.client.models.v1_job_status import V1JobStatus
+from kubernetes.client.api.batch_v1_api import BatchV1Api
+from kubernetes.client.models.v1_env_var import V1EnvVar
 import ryaml
 
 import doe
 
 
 app = typer.Typer()
-
-
 JOB_NAME = "dnsperf-test-wrapper"
-
-from kubernetes.dynamic import DynamicClient
-from kubernetes.client.models.v1_job_status import V1JobStatus
-from kubernetes.client.api.batch_v1_api import BatchV1Api
-from kubernetes.client.models.v1_env_var import V1EnvVar
-
 k8s_job_attribute_map = {
     val: key for key, val in V1JobStatus.attribute_map.items()
 }
@@ -34,7 +26,7 @@ def create_job_object(job_args, es, es_index):
         image='quay.io/mleader/dnsdebug:latest',
         image_pull_policy = 'Always',
         command=["/bin/sh", "-c"],
-        args=[' '.join(("snafu/run_snafu.py", "-v", "--tool", "dnsperf", *job_args))],
+        args=[' '.join(("python", "snafu/run_snafu.py", "-v", "--tool", "dnsperf", *job_args))],
         env=[V1EnvVar(name='es', value=es), V1EnvVar(name='es_index', value=es_index)]
         )
     # Create and configurate a spec section
@@ -82,33 +74,10 @@ def delete_job(api_instance):
     print("Job deleted. status='%s'" % str(api_response.status))
 
 
-def create_pod(env):
-    return client.V1Pod(
-        api_version = "v1",
-        kind = "Pod",
-        metadata = client.V1ObjectMeta(
-            name = env['name'],
-            namespace = env['namespace']
-        ),
-        spec = client.V1PodSpec(
-            containers = [
-                client.V1Container(
-                    name = env['container']['name'],
-                    image = env['container']['image'],
-                    image_pull_policy = 'Always',
-                    command = env['container']['command'],
-                    args = env['container']['args']
-                )
-            ],
-            restart_policy = 'Never'
-        )
-    )
-
-
-def wait_on_job(trial, api_client, es_url, es_index):
+def wait_on_job(trial, api_client, es, es_index, sleep_t):
     trial_args = doe.serialize_command_args(trial)
     batch_v1 = BatchV1Api(api_client=api_client)
-    job = create_job_object(trial_args, es=es_url, es_index=es_index)
+    job = create_job_object(trial_args, es=es, es_index=es_index)
     api_dynamic = DynamicClient(api_client)
     job_resources = api_dynamic.resources.get(api_version='v1', kind='Job')
 
@@ -116,24 +85,26 @@ def wait_on_job(trial, api_client, es_url, es_index):
     create_job(batch_v1, job)
     watcher = watch.Watch()
 
-    # needs to be async too
+    # probably should be async
     for event in api_dynamic.watch(job_resources, namespace='dnsperf-test', watcher=watcher):
         j = V1JobStatus(**{k8s_job_attribute_map[key]: val for key,val in event['raw_object']['status'].items() })
-        print('======================================================')
+        print('------------------------------------------------------')
+        pprint(f'job condition: {j.conditions}')
         if j.succeeded:
+            print('======================================================')
             print('SUCCESS!')
             watcher.stop()
 
-    # needs to be async
+    # probably should be async
     delete_job(batch_v1)
-    time.sleep(3)
+    time.sleep(sleep_t)
 
 
 @app.command()
 def main(
     experiment_factor_levels_path: str = typer.Argument(...),
-    es_url: str = typer.Option(...),
-    es_index: str = typer.Option(...),
+    es: str = typer.Option(...),
+    es_index: str = typer.Option('snafu-dnsperf'),
     sdn_kubeconfig_path: str = typer.Option(...),
     ovn_kubeconfig_path: str = typer.Option(...),
     sleep_t: int = typer.Option(2),
@@ -142,71 +113,16 @@ def main(
     sdn_cluster = config.new_client_from_config(sdn_kubeconfig_path)
     ovn_cluster = config.new_client_from_config(ovn_kubeconfig_path)
 
-    # myargs = [
-    #     "python",
-    #     "snafu/run_snafu.py",
-    #     "--tool",
-    #     "dnsperf",
-    #     "-v",
-    #     "--run-id",
-    #     "88ff59df-117c-46ac-ae4b-ea5962869387",
-    #     "--block-id",
-    #     "1",
-    #     "--load_limit",
-    #     "inf",
-    #     "--query_path",
-    #     "./dnsdebug/noerror.txt",
-    #     "--control_plane_nodes",
-    #     "3",
-    #     "--network_type",
-    #     "OpenShiftSDN",
-    #     "--transport_mode",
-    #     "tcp",
-    #     "--client_threads",
-    #     "1",
-    #     "--trial_id",
-    #     "8",
-    #     "--runtime-length",
-    #     "2"
-    # ]
 
     for trial in doe.main(factor_levels_filepath=experiment_factor_levels_path, block_id=block_id):
         # pprint(trial)
-
         if trial['network_type'] == 'OpenShiftSDN':
-            wait_on_job(trial, sdn_cluster, es_url, es_index)
+            wait_on_job_api = partial(wait_on_job, api_client=sdn_cluster)
         elif trial['network_type'] == 'OVNKubernetes':
-            wait_on_job(trial, ovn_cluster, es_url, es_index)
+            wait_on_job_api = partial(wait_on_job, api_client=ovn_cluster)
 
+        wait_on_job_api(trial, es=es, es_index=es_index, sleep_t=sleep_t)
 
-    # batch_v1 = BatchV1Api(api_client=sdn_cluster)
-    # job = create_job_object(myargs)
-    # sdn_dynamic = DynamicClient(sdn_cluster)
-    # job_resources = sdn_dynamic.resources.get(api_version='v1', kind='Job')
-
-    # pprint(job)
-    # print(ryaml.dumps(sdn_cluster.sanitize_for_serialization(job)))
-    # create_job(batch_v1, job)
-    # watcher = watch.Watch()
-
-    # for event in sdn_dynamic.watch(job_resources, namespace='dnsperf-test', watcher=watcher):
-    #     j = V1JobStatus(**{k8s_job_attribute_map[key]: val for key,val in event['raw_object']['status'].items() })
-    #     print('======================================================')
-    #     if j.succeeded:
-    #         print('SUCCESS!')
-    #         watcher.stop()
-            # delete_job(batch_v1)
-
-
-    # for event in sdn_ocp_pods.watch(namespace='dnsperf-test'):
-    #     # print(str(event['object']))
-    #     e = ryaml.loads(str(event['object']))
-    #     # print(e)
-    #     print(e['ResourceInstance[Pod]']['metadata']['name'])
-    #     print(e['ResourceInstance[Pod]']['status']['phase'])
-    #     if e['ResourceInstance[Pod]']['status']['phase'] == 'Succeeded':
-    #         print(e['ResourceInstance[Pod]']['status']['containerStatuses'][-1]['state']['terminated']['reason'])
-    #     print('-----------------------')
 
 
 
