76439c2078b7d971e8a587554353669139da854b
mleader <mleader@redhat.com>
2021-09-29 15:43:45 -0400

    start async

--- async_main.py
+++ async_main.py
@@ -6,11 +6,11 @@ import datetime as dt
 
 import pytz
 import typer
-from kubernetes import client, config, watch
-from kubernetes.dynamic import DynamicClient
-from kubernetes.client.models.v1_job_status import V1JobStatus
-from kubernetes.client.api.batch_v1_api import BatchV1Api
-from kubernetes.client.models.v1_env_var import V1EnvVar
+
+from kubernetes_asyncio import client, config, watch
+from kubernetes_asyncio.client.models.v1_job_status import V1JobStatus
+from kubernetes_asyncio.client.api.batch_v1_api import BatchV1Api
+from kubernetes_asyncio.client.models.v1_env_var import V1EnvVar
 
 import ryaml
 
@@ -55,25 +55,25 @@ def create_job_object(job_args, es, es_index):
     return job
 
 
-def create_job(api_instance, job):
-    api_response = api_instance.create_namespaced_job(
+async def create_job(api_instance, job):
+    api_response = await api_instance.create_namespaced_job(
         body=job,
         namespace="dnsperf-test")
     print("Job created. status='%s'" % str(api_response.status))
 
 
-def update_job(api_instance, job):
+async def update_job(api_instance, job):
     # Update container image
     job.spec.template.spec.containers[0].image = "perl"
-    api_response = api_instance.patch_namespaced_job(
+    api_response = await api_instance.patch_namespaced_job(
         name=JOB_NAME,
         namespace="dnsperf-test",
         body=job)
     print("Job updated. status='%s'" % str(api_response.status))
 
 
-def delete_job(api_instance):
-    api_response = api_instance.delete_namespaced_job(
+async def delete_job(api_instance):
+    api_response = await api_instance.delete_namespaced_job(
         name=JOB_NAME,
         namespace="dnsperf-test",
         body=client.V1DeleteOptions(
@@ -82,32 +82,26 @@ def delete_job(api_instance):
     print("Job deleted. status='%s'" % str(api_response.status))
 
 
-def wait_on_job(trial, api_client, es, es_index, sleep_t):
+async def job_exec(trial, api_client, es, es_index, sleep_t):
     trial_args = doe.serialize_command_args(trial)
     batch_v1 = BatchV1Api(api_client=api_client)
     job = create_job_object(trial_args, es=es, es_index=es_index)
-    api_dynamic = DynamicClient(api_client)
-    job_resources = api_dynamic.resources.get(api_version='v1', kind='Job')
-    watcher = watch.Watch()
+    # job_resources = api_dynamic.resources.get(api_version='v1', kind='Job')
+    # watcher = watch.Watch()
 
     # print(ryaml.dumps(api_client.sanitize_for_serialization(job)))
-    create_job(batch_v1, job)
-
-    # probably should be async
-    for event in api_dynamic.watch(job_resources, namespace='dnsperf-test', watcher=watcher):
-        j = V1JobStatus(**{k8s_job_attribute_map[key]: val for key,val in event['raw_object']['status'].items()})
-        print('------------------------------------------------------')
-        pprint(f'job condition: {j.conditions}')
-        if j.succeeded:
-            watcher.stop()
-
-    # probably should be async
-    delete_job(batch_v1)
-    time.sleep(sleep_t)
+    await create_job(batch_v1, job)
+    await watch_job(api_client)
+    await delete_job(batch_v1)
+    await anyio.sleep(sleep_t)
 
 
 async def watch_job(api_client):
-
+    async with watch.Watch().stream(api_client.resources.get(api_version='v1', kind='Job'), namespace='dnsperf-test') as stream:
+        async for event in stream:
+            j = V1JobStatus(**{k8s_job_attribute_map[key]: val for key,val in event['raw_object']['status'].items()})
+            print('------------------------------------------------------')
+            pprint(f'job condition: {j.conditions}')
 
 
 async def _experiment(
@@ -121,11 +115,14 @@ async def _experiment(
     replicate: int,
     measure_repetitions: int
 ):
-    k8s_sdn_api = config.new_client_from_config(sdn_kubeconfig_path)
-    k8s_ovn_api = config.new_client_from_config(ovn_kubeconfig_path)
+    k8s_sdn_api = await config.new_client_from_config(sdn_kubeconfig_path)
+    k8s_ovn_api = await config.new_client_from_config(ovn_kubeconfig_path)
+    k8s_client_apis = {
+        'OpenShiftSDN': k8s_sdn_api,
+        'OVNKubernetes': k8s_ovn_api
+    }
     trial_times = []
     completed_trials = 0
-    eastern = pytz.timezone('US/Eastern')
 
     trials = [t for t in doe.main(factor_levels_filepath=experiment_factor_levels_path, block=block)]
     total_trials = len(trials)
@@ -135,12 +132,14 @@ async def _experiment(
         pprint(input_args['trial'])
         trial_start = dt.datetime.now()
 
-        if input_args['trial']['network_type'] == 'OpenShiftSDN':
-            wait_on_job_api = partial(wait_on_job, api_client=k8s_sdn_api)
-        elif input_args['trial']['network_type'] == 'OVNKubernetes':
-            wait_on_job_api = partial(wait_on_job, api_client=k8s_ovn_api)
+        # if input_args['trial']['network_type'] == 'OpenShiftSDN':
+        #     wait_on_job_api = partial(wait_on_job, api_client=k8s_sdn_api)
+        # elif input_args['trial']['network_type'] == 'OVNKubernetes':
+        #     wait_on_job_api = partial(wait_on_job, api_client=k8s_ovn_api)
 
-        wait_on_job_api({**input_args['common'], **input_args['trial']}, es=es, es_index=es_index, sleep_t=sleep_t)
+        await job_exec({**input_args['common'], **input_args['trial']},
+            api_client=k8s_client_apis[input_args['trial']['network_type']],
+            es=es, es_index=es_index, sleep_t=sleep_t)
 
         trial_end = dt.datetime.now()
         completed_trials += 1
